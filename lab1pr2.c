e56daf8475c1055da0f8c4a0b0ad3832d09ffe0c
arthurqchu <arthurqc@gmail.com>
2020-01-23 14:51:33 -0600

    Finished the first problem

--- lab1pr2.c
+++ lab1pr2.c
@@ -1,8 +1,32 @@
 //Arthur Chu
-
+#include <stdio.h>
+#include <stdbool.h>
 bool isHex(char* string) {
-	while(char* ch != '0') {
+	char* ch; 
+	while(*ch != '\0') {
 		ch = string;
-		if ch >= '0' && ch <= '9'
+		if(*ch >= '0' && *ch <= '9') {
+			ch++;
+		}
+		if(*ch >= 'A' && *ch <= 'F') {
+			ch++;
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(void) {
+	bool done = false;
+	while(done == false) {
+		printf("Please enter a hexadecimal number: \n");
+		char input[20];
+		scanf("%s", input);
+		if(isHex(input) == true) {
+			printf("Thank you!");
+			done = true;
+		}
 	}
 }
