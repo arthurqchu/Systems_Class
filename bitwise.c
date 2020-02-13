e0b67496dc66a75a34b6a6d0381bf947626d9edb
arthurqchu <arthurqc@gmail.com>
2020-02-12 23:37:17 -0600

    DONE

--- bitwise.c
+++ bitwise.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 //Arthur Chu
 int prefix(unsigned int x, unsigned int n) {
-    int nRightShifted = x >> (32 - n);
-    printf("The number in decimal form is %d\n", nRightShifted);
-    int temp;
-    for (int i = 32; i > 0; i--) {
-        temp = x >> i;
-
-        if (temp & 1) {
-            printf("1");
-        } else {
-            printf("0");
-        }
-    }
-    printf("\n");
+    int nRightShifted = x >> (16 - n);
     return nRightShifted;
 }
 
 int suffix(unsigned int x, unsigned int n) {
-    int nLeftShifted = x << (32 - n);
-    printf("The number in decimal form is %d\n", nLeftShifted);
+    int nLeftShifted = x << (16 - n);
+    return nLeftShifted;
+}
+
+int toggle(unsigned int x, unsigned int n) {
+    return (x | (1 << (n -1)));
+}
+
+void main() {
+    unsigned int a = 10000, b = 10;
+    printf("a = %d\nb = %d\n", a, b);
+
+    int p = prefix(a, 4);
+    int s = suffix(b, 4);
     int temp;
-    for (int i = 32; i > 0; i--) {
-        temp = x << i;
+    printf("a (after prefix) = %d. In binary it's ", p);
+    for (int i = 16; i > 0; i--) {
+        temp = p >> i;
 
         if (temp & 1) {
             printf("1");
@@ -31,14 +32,16 @@ int suffix(unsigned int x, unsigned int n) {
         }
     }
     printf("\n");
-    return nLeftShifted;
-}
+    printf("b (after suffix)= %d. In binary it's ", s);
+    for (int i = 16; i > 0; i--) {
+        temp = s >> i;
 
-int toggle(unsigned int x, unsigned int n) {
-    return (x | (1 << (n -1));
-}
-
-void main() {
-
+        if (temp & 1) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+    }
+    printf("\n");
 return;
 }
